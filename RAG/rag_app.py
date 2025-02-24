7f5e817bdb819d845ba0e11e8ecb3a79491e1ed8
carlgomv <carlos.gomzv@gmail.com>
2025-02-24 11:06:24 -0500

    Notebook: modified to run on my mac. Rag_app: modified to show the name of the collection which is been analyzed

--- RAG/rag_app.py
+++ RAG/rag_app.py
@@ -22,15 +22,18 @@ class AI():
 		res_db = self.collection.query(query_texts=[q])["documents"][0][0:top]
 		context = ' '.join(res_db).replace("\n", " ")
 		return context
+      
+	def getCollection(self):
+		return self.collection.name
 
 	def respond(self, lst_messages, model="phi3", use_knowledge=False):
 		q = lst_messages[-1]["content"]
 		context = self.query(q)
 
 		if use_knowledge:
-			prompt = "Give the most accurate answer using your knowledge and the folling additional information: \n"+context
+			prompt = "Give the most accurate answer using your knowledge and the following additional information: \n"+context
 		else:
-			prompt = "Give the most accurate answer using only the folling information: \n"+context
+			prompt = "Give the most accurate answer using only the following information: \n"+context
 
 		res_ai = ollama.chat(model=model, 
 							 messages=[{"role":"system", "content":prompt}]+lst_messages,
@@ -45,7 +48,7 @@ ai = AI()
 
 ######################## Frontend #############################
 ## Layout
-st.title('💬 Write your questions')
+st.title('💬 Write your questions about the document ' + ai.getCollection())
 st.sidebar.title("Chat History")
 app = st.session_state
 
